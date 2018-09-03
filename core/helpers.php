ffe2bbcc52a06b6b4f1cfd49c3d9e2d2ce6e45a0
Juliano Petronetto <juliano.petronetto@gmail.com>
2018-09-03 15:46:04 -0300

    Added more helpers

--- core/helpers.php
+++ core/helpers.php
@@ -2,6 +2,83 @@
 
 declare(strict_types=1);
 
+if (!function_exists('container')) {
+    /**
+     * Get the application container.
+     *
+     * @return Psr\Container\ContainerInterface
+     */
+    function container(): Psr\Container\ContainerInterface
+    {
+        return require base_path('bootstrap/init.php');
+    }
+}
+
+if (!function_exists('config')) {
+    /**
+     * Get the application container.
+     *
+     * @return Core\Config\ConfigInterface
+     */
+    function config(): Core\Config\ConfigInterface
+    {
+        return container()->get(Core\Config\ConfigInterface::class);
+    }
+}
+
+if (!function_exists('storage')) {
+    /**
+     * Get the application storage.
+     *
+     * @return Core\Storage\StorageInterface
+     */
+    function storage(): Core\Storage\StorageInterface
+    {
+        return container()->get(Core\Storage\StorageInterface::class);
+    }
+}
+
+if (!function_exists('flash')) {
+    /**
+     * Create a flash message in application storage.
+     *
+     * @return void
+     */
+    function flash(string $message): void
+    {
+        storage()->set('app_flash_message', $message);
+    }
+}
+
+if (!function_exists('has_flash')) {
+    /**
+     * Create a message in application storage.
+     *
+     * @return bool
+     */
+    function has_flash(): bool
+    {
+        if (storage()->has('app_flash_message')) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('print_flash')) {
+    /**
+     * Print some text.
+     *
+     * @return void
+     */
+    function print_flash(): void
+    {
+        echo storage()->get('app_flash_message');
+        storage()->delete('app_flash_message');
+    }
+}
+
 if (!function_exists('base_path')) {
     /**
      * Get the application base path.
@@ -19,16 +96,20 @@ if (!function_exists('base_path')) {
 
 if (!function_exists('base_url')) {
     /**
-     * Get the application base url.
+     * Get the application base url + the path if provided.
      *
+     * @param  string $path
      * @return string
      */
-    function base_url(): string
+    function base_url(string $path = null): string
     {
+        ($path !== '/') ?: $path = null;
+
         return sprintf(
-            '%s:%s',
-            getenv('APP_URL'),
-            getenv('APP_PORT')
+            '%s:%s/%s',
+            config()->get('app.url'),
+            config()->get('app.port'),
+            $path
         );
     }
 }
@@ -43,10 +124,28 @@ if (!function_exists('assets')) {
     function assets(string $file): string
     {
         return sprintf(
-            '%s/assets/%s',
+            '%sassets/%s',
             base_url(),
             $file
         );
+    }
+}
+
+if (!function_exists('hex_to_str')) {
+    /**
+     * Convert hex to string.
+     *
+     * @param  string  $hex
+     * @return string
+     */
+    function hex_to_str(string $hex): string
+    {
+        $string = '';
+        for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+            $string .= chr(hexdec($hex[$i].$hex[$i + 1]));
+        }
+
+        return $string;
     }
 }
 
